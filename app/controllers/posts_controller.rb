1b9dca8aeef853b1189f1ae7c35e66d929cf7aa6
Brian Ildefonso <brianildefonso@Brians-MacBook-Pro.local>
2024-06-11 12:03:46 -0400

    save all files

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+ class PostsController < ApplicationController
+  def create
+    # Implement the logic to create a new post
+  end
+
+  def index
+   # Implement the logic to list all posts
+  end
+   def update
+   # Implement the logic to modify a post identified by the post's ID
+  end
+
+  def destroy
+     # Implement the logic to delete a post identified by the post's ID
+ end
+end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,5 @@
+ class UsersController < ApplicationController
+  def create
+    # Implement the logic to create a new user
+  end
+end
